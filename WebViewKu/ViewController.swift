b6998ff1b5dbc766bf4a071a610e82b916a9f953
zidniryi <zidnirydwan@gmail.com>
2018-08-13 10:09:44 +0700

    Initial Commit

---
+++ WebViewKu/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WebViewKu
+//
+//  Created by hint on 09/08/18.
+//  Copyright © 2018 SlametRiyadi. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var WebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string : "http://www.jooinfoo.com")
+        let request = URLRequest(url: url!)
+        
+        
+        WebView.load(request)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
